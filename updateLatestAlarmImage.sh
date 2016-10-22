da1f23a5d3e2c68c7ae705f14d4a35e54769cec6
Shoo Kyuk Wei <acefreakz@gmail.com>
2016-10-23 00:21:43 +0800

    Rename symlink latestAlarmImage to .latestAlarmImage. This file should be generated locally.

--- updateLatestAlarmImage.sh
+++ updateLatestAlarmImage.sh
@@ -2,11 +2,11 @@
 
 # 
 latestAlarmFilename=`grep "\[iptvuser\] OK UPLOAD: Client \"192.168.0.7\"" /var/log/vsftpd.log | grep -oE "/files.*A[0-9]*\.jpg" | tail -n 1`
-echo "DEBUG: latestAlarmFilename = "$latestAlarmFilename
+echo "DEBUG: .latestAlarmFilename symlinked to: "$latestAlarmFilename
 
-if [[ `readlink -f latestAlarmImage` != *$latestAlarmFilename ]]; then
+if [[ `readlink -f .latestAlarmImage` != *$latestAlarmFilename ]]; then
 	echo "DEBUG: latestAlarmImage outdated, updating symlink!"
-	ln -fs /home/iptvuser$latestAlarmFilename latestAlarmImage	
+	ln -fs /home/iptvuser$latestAlarmFilename .latestAlarmImage
 else
 	echo "DEBUG: latestAlarmImage same file! No need update symlink!"
 fi 
