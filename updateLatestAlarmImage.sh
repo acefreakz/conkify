8d49ca7743161257ea37a5d9fdbd5ddad8081871
Shoo Kyuk Wei <acefreakz@gmail.com>
2016-10-22 13:22:08 +0800

    Bug fix: To get only latest alarm file (starts with A[0-9]*.jpg. We don't want manual triggered image file to be used.

--- updateLatestAlarmImage.sh
+++ updateLatestAlarmImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 
-latestAlarmFilename=`grep "\[iptvuser\] OK UPLOAD: Client \"192.168.0.7\"" /var/log/vsftpd.log | tail -n 1 | grep -oE "/files.*jpg"`
+latestAlarmFilename=`grep "\[iptvuser\] OK UPLOAD: Client \"192.168.0.7\"" /var/log/vsftpd.log | grep -oE "/files.*A[0-9]*\.jpg" | tail -n 1`
 echo "DEBUG: latestAlarmFilename = "$latestAlarmFilename
 
 if [[ `readlink -f latestAlarmImage` != *$latestAlarmFilename ]]; then
